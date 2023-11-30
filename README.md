966701567d336aba73e091576ccfc0890d8d369e
Peter Hamer <peterhamer@Peters-Air.lan>
2023-11-30 19:22:42 +0000

    Day 0: Set Up Repo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # advent-of-code-2023
 https://adventofcode.com/
+
+## How to run
+`cd dir`
+`npx ts-node <day_>.ts`
---
+++ src/days/day1/day1.ts
@@ -0,0 +1,13 @@
+import readFile from "../../util/readFile";
+
+const solve_ex1 = async () => {
+  const puzzleInput = await readFile(`./input.txt`);
+
+}
+
+const solve_ex2 = async () => {
+  const puzzleInput = await readFile(`./input.txt`);
+}
+
+solve_ex1()
+solve_ex2()
