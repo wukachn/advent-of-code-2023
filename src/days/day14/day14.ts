80c01949812f63bc0923f248668599e6807917cd
wukachn <peter.hamerrr@gmail.com>
2023-12-14 08:51:13 +0000

    Day 14: Parabolic Reflector Dish

--- src/days/day14/day14.ts
+++ src/days/day14/day14.ts
@@ -1,11 +1,33 @@
 import readFile from "../../util/readFile"
 import { transpose } from "../../util/matrix"
 
-const rollNorth = (platform: string[][]): string[][] => {
+const memoize = require('just-memoize')
+
+const rollForward = (platform: string[][]): string[][] => {
     platform = transpose(platform)
     platform.forEach((column, id) => platform[id] = column.join('').split('#').map(part => part.split('').sort().reverse().join('')).join('#').split(''))
     return transpose(platform)
 }
+
+const rollBackwards = (platform: string[][]): string[][] => {
+    platform = transpose(platform)
+    platform.forEach((column, id) => platform[id] = column.join('').split('#').map(part => part.split('').sort().join('')).join('#').split(''))
+    return transpose(platform)
+}
+
+const rollLeft = (platform: string[][]): string[][] => {
+    platform.forEach((row, id) => platform[id] = row.join('').split('#').map(part => part.split('').sort().reverse().join('')).join('#').split(''))
+    return platform
+}
+
+const rollRight = (platform: string[][]): string[][] => {
+    platform.forEach((row, id) => platform[id] = row.join('').split('#').map(part => part.split('').sort().join('')).join('#').split(''))
+    return platform
+}
+
+const rollCycle = memoize((platform: string[][]): string[][] => {
+    return rollRight(rollBackwards(rollLeft(rollForward(platform))))
+})
 
 const calculateTotalLoad = (platform: string[][]): number => {
     let total: number = 0
@@ -22,7 +44,24 @@ const calculateTotalLoad = (platform: string[][]): number => {
 const run = async () => {
     let platform: string[][] = await (await readFile(`./input.txt`)).split('\n').map(line => line.split(''))
 
-    console.log("Ex 1: " + calculateTotalLoad(rollNorth(platform))) // 113525
+    console.log("Ex 1: " + calculateTotalLoad(rollForward(platform))) // 113525
+
+    const platfromHistory: string[][][] = []
+    let cycle: number = 0
+    while (cycle < 1000000000) {
+        platform = rollCycle(platform)
+        if (platfromHistory.includes(platform)) {
+            break
+        }
+        platfromHistory.push(platform)
+        cycle += 1
+    }
+    const cycleStartIndex: number = platfromHistory.indexOf(platform)
+    const cyclesLeft: number = (1000000000 - cycleStartIndex) % (cycle - cycleStartIndex) - 1
+    for (cycle = 0; cycle < cyclesLeft; cycle++) {
+        platform = rollCycle(platform) // 101292
+    }
+    console.log("Ex 2: " + calculateTotalLoad(platform))
 }
 
 run()
